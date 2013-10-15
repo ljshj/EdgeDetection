e5c34579f9c9cbe38cde0a340ac0ee25c68e2699
kir <p-kir@yandex.com>
2013-10-14 21:37:04 -0700

    - opencv for iOS

---
+++ EdgeDetection/UIImage+OpenCV.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+OpenCV.h
+//  EdgeDetection
+//
+//  Created by Kirill Pugin on 10/14/13.
+//  Copyright (c) 2013 pkir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (OpenCV)
+
+@end
---
+++ EdgeDetection/UIImage+OpenCV.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+OpenCV.m
+//  EdgeDetection
+//
+//  Created by Kirill Pugin on 10/14/13.
+//  Copyright (c) 2013 pkir. All rights reserved.
+//
+
+#import "UIImage+OpenCV.h"
+
+@implementation UIImage (OpenCV)
+
+@end
